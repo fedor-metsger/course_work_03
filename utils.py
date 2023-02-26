c8d5096be738e625deb8ea7e0ff06af273e36423
Fedor Metsger <fedor.metsger@gmail.com>
2023-02-26 15:30:24 +0300

    Clean up code

--- utils.py
+++ utils.py
@@ -1,6 +1,7 @@
 
 import json
 
+
 def load_transactions(filename: str):
     """
     Загружает список транзакций из файла
@@ -14,9 +15,10 @@ def load_transactions(filename: str):
         if tr_list:
             print("INFO: Данные загружены успешно")
             return tr_list
-    except:
-        print("ERROR: Ошибка при загрузке транзакций")
+    except Exception as e:
+        print("ERROR: Ошибка при загрузке транзакций из файла:", repr(e))
         return None
+
 
 def get_filtered(transactions, state):
     """
@@ -26,12 +28,8 @@ def get_filtered(transactions, state):
     """
     res = []
     for i in transactions:
-        try:
-            if i["state"] == state:
-                res.append(i)
-        except Exception as e:
-            pass
-#            print( "get_filtered:ERROR:", repr(e))
+        if "state" in i and i["state"] == state:
+            res.append(i)
     return res
 
 
@@ -47,12 +45,13 @@ def get_first(transactions, cnt):
     Сортирует список транзакций по полю "date"
     Возвращает нужное количество записей от начала списка
     """
-    transactions.sort(key = get_date, reverse = True)
+    transactions.sort(key=get_date, reverse=True)
     return transactions[:cnt]
 
 
 def format_from_to(in_str):
-    if in_str[0:4] == "Счет": return "Счет **" + in_str[-4:]
+    if in_str.split()[0] == "Счет":
+        return "Счет **" + in_str[-4:]
     else:
         name, num = ' '.join(in_str.split()[:-1]), in_str.split()[-1]
         num = f"{num[0:4]} {num[4:6]}** **** {num[12:16]}"
@@ -65,18 +64,14 @@ def get_formatted(transaction):
     Возвращает их в нужном формате
     """
     date = '.'.join([transaction["date"][5:7], transaction["date"][8:10], transaction["date"][0:4]])
-    descr = transaction["description"]
-    tr_from = "[Нет данных]"
-    try:
+    descr, tr_from, tr_to = "[Описание отсутствует]", "[Нет данных]", "[Нет данных]"
+    if "description" in transaction:
+        descr = transaction["description"]
+    if "from" in transaction:
         tr_from = format_from_to(transaction["from"])
-    except:
-        pass
-#    if tr_from[0:4] == "Счет": tr_from = "Счет **" + tr_from[-4:]
-    tr_to = "[Нет данных]"
-    try:
+    if "to" in transaction:
         tr_to = format_from_to(transaction["to"])
-    except:
-        pass
+
     amount = transaction["operationAmount"]["amount"]
     curr = transaction["operationAmount"]["currency"]["name"]
 
